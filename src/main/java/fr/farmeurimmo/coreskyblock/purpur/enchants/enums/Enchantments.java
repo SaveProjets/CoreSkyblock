004c0efc492038aa2cc8d9020911909f4b7fa141
Farmeurimmo <contact@farmeurimmo.fr>
2024-08-06 14:19:39 +0200

    Better design of Enchantments (requested by Mar)

--- src/main/java/fr/farmeurimmo/coreskyblock/purpur/enchants/enums/Enchantments.java
+++ src/main/java/fr/farmeurimmo/coreskyblock/purpur/enchants/enums/Enchantments.java
@@ -10,81 +10,81 @@ import java.util.List;
 
 public enum Enchantments {
 
-    GAIN_DE_VIE("Gain de vie", "§fPermet de gagner {value} cœurs supplémentaires.",
+    GAIN_DE_VIE("Gain de vie", "§8Permet de gagner {value} cœurs supplémentaires.",
             2, false, 1, 1, 0, -1,
             EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.HELMET, EnchantmentsRecipients.CHESTPLATE,
             EnchantmentsRecipients.LEGGINGS, EnchantmentsRecipients.BOOTS)),
 
-    RENVOI("Renvoi", "§fChance de {value}% de rediriger l’attaque de l’adversaire contre lui-même.",
+    RENVOI("Renvoi", "§8Chance de {value}% de rediriger l’attaque de l’adversaire contre lui-même.",
             2, true, 0.02, 1, 0, -1,
             EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.HELMET, EnchantmentsRecipients.CHESTPLATE,
             EnchantmentsRecipients.LEGGINGS, EnchantmentsRecipients.BOOTS)),
 
-    LAMPE_TORCHE("Lampe torche", "§fPermet d’avoir l’effet vision nocturne", -1, false,
+    LAMPE_TORCHE("Lampe torche", "§8Permet d’avoir l’effet vision nocturne", -1, false,
             0, 0, 0, -1, EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.HELMET)),
 
-    DECAPITEUR("Décapiteur", "§fQuand on tue un joueur avec une épée avec l’enchantement, il y a {value}% que la tête du joueur tuée drop au sol",
+    DECAPITEUR("Décapiteur", "§8Quand on tue un joueur avec une épée avec l’enchantement, il y a {value}% que la tête du joueur tuée drop au sol",
             4, true, 0.025, 1, 0, -1,
             EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.SWORD)),
 
-    SEISME("Séisme", "§fPermet de repousser votre adversaire et de l’envoyer en l’air en faisant clic droit, cooldown de {value_cooldown} secondes.",
+    SEISME("Séisme", "§8Permet de repousser votre adversaire et de l’envoyer en l’air en faisant clic droit, cooldown de {value_cooldown} secondes.",
             2, false, 0, 0, 15, 5,
             EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.SWORD)),
 
-    TEMPETE_DE_FOUDRE("Tempête de foudre", "§fChance de {value}% que la foudre apparaît pour frapper l’adversaire. La foudre: 2 points de dégâts et inflige 3 dégâts à la durabilité de l’armure.",
+    TEMPETE_DE_FOUDRE("Tempête de foudre", "§8Chance de {value}% que la foudre apparaît pour frapper l’adversaire. La foudre: 2 points de dégâts et inflige 3 dégâts à la durabilité de l’armure.",
             2, true, 0.02, 1, 0, -1,
             EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.SWORD)),
 
-    GELURE("Gelure", "§fChance de {value}% de paralyser l'ennemi (effet de slowness extrêmement important) durant 2 à 4 secondes.",
+    GELURE("Gelure", "§8Chance de {value}% de paralyser l'ennemi (effet de slowness extrêmement important) durant 2 à 4 secondes.",
             3, true, 0.02, 0.5, 0, -1,
             EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.SWORD)),
 
-    INTIMIDATION("Intimidation", "§fChance de {value}% d’appliquer l’effet faiblesse à l’adversaire pendant {value_cooldown} secondes.",
+    INTIMIDATION("Intimidation", "§8Chance de {value}% d’appliquer l’effet faiblesse à l’adversaire pendant {value_cooldown} secondes.",
             2, true, 0.02, 0.5, 0, -1,
             EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.SWORD)),
 
-    XP_TRANSFORMATEUR("Xp transformateur", "§fPermet de transformer le loot des mobs lors de leur mort en xp vanilla. (+10% d’xp).",
+    XP_TRANSFORMATEUR("Xp transformateur", "§8Permet de transformer le loot des mobs lors de leur mort en xp vanilla. (+10% d’xp).",
             -1, true, 10, 0.0, 0, -1,
             EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.SWORD)),
 
-    AIMANT("Aimant", "§fPermet que blocs minés, cassés ou autre ainsi que le drop des mobs tués (avec un item avec cette enchantement) arrive directement dans l’inventaire.",
+    AIMANT("Aimant", "§8Permet que blocs minés, cassés ou autre ainsi que le drop des mobs tués (avec un item avec cette enchantement) arrive directement dans l’inventaire.",
             -1, false, 0, 0, 0, -1,
             EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.SWORD, EnchantmentsRecipients.PICKAXE,
             EnchantmentsRecipients.AXE, EnchantmentsRecipients.HOE, EnchantmentsRecipients.ROD, EnchantmentsRecipients.SHOVEL)),
 
-    GEYSER("Geyser", "§fChance de {value}% d’envoyer légèrement l’adversaire en l’air en faisant apparaître un geyser.",
+    GEYSER("Geyser", "§8Chance de {value}% d’envoyer légèrement l’adversaire en l’air en faisant apparaître un geyser.",
             2, true, 0.02, 1, 0, -1,
             EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.SWORD)),
 
-    TIR_EXPLOSIF("Tir explosif", "§fChance de {value}% que la flèche tirée produise une explosion qui fait 1,5 fois plus puissant que l’attaque normale de l’arc.",
+    TIR_EXPLOSIF("Tir explosif", "§8Chance de {value}% que la flèche tirée produise une explosion qui fait 1,5 fois plus puissant que l’attaque normale de l’arc.",
             5, true, 0.02, 1, 0, -1,
             EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.BOW_AND_CROSSBOW)),
 
-    FLECHE_GELEE("Flèche gelée", "§fChance de {value}% de paralyser l'ennemi (effet de slowness extrêmement important) durant {value_effect} secondes.",
+    FLECHE_GELEE("Flèche gelée", "§8Chance de {value}% de paralyser l'ennemi (effet de slowness extrêmement important) durant {value_effect} secondes.",
             3, true, 0.03, 0.5, 0, 2,
             EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.BOW_AND_CROSSBOW)),
 
-    FLECHE_TONNERRE("Flèche tonnerre", "§fChance de {value}% que la foudre apparaît pour frapper l’adversaire. La foudre: 2 points de dégâts et inflige 3 dégâts à la durabilité de l’armure.",
+    FLECHE_TONNERRE("Flèche tonnerre", "§8Chance de {value}% que la foudre apparaît pour frapper l’adversaire. La foudre: 2 points de dégâts et inflige 3 dégâts à la durabilité de l’armure.",
             2, true, 0.03, 1, 0, -1,
             EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.BOW_AND_CROSSBOW)),
 
-    SNIPER("Sniper", "§fChance de {value}% de doubler les dégâts de l’arc.", 3, true,
+    SNIPER("Sniper", "§8Chance de {value}% de doubler les dégâts de l’arc.", 3, true,
             0.02, 1, 0, -1, EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.BOW_AND_CROSSBOW)),
 
-    PLUIE_DE_FLECHES("Pluie de flèches", "§fChance de {value}% que des flèches apparaissent autour de l’adversaire et foncent sur lui.",
+    PLUIE_DE_FLECHES("Pluie de flèches", "§8Chance de {value}% que des flèches apparaissent autour de l’adversaire et foncent sur lui.",
             3, true, 0.02, 0.5, 0, -1,
             EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.BOW_AND_CROSSBOW)),
 
-    SMELTING("Smelting", "§fPermet de faire fondre automatiquement les minerais minés.",
+    SMELTING("Smelting", "§8Permet de faire fondre automatiquement les minerais minés.",
             -1, false, 0, 0, 0, -1, EnchantmentRarity.UNCOMMON, List.of(EnchantmentsRecipients.PICKAXE)),
 
-    ORE_XP("Ore Xp", "§fPermet de multiplier l’xp reçue en cassant des minerais.", 1,
+    ORE_XP("Ore Xp", "§8Permet de multiplier l’xp reçue en cassant des minerais.", 1,
             false, 0, 0, 0, -1, EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.PICKAXE)),
 
-    LASSO("Lasso", "§fPermet d’augmenter la puissance de grab de la rod (pvp).", -1,
+    LASSO("Lasso", "§8Permet d’augmenter la puissance de grab de la rod (pvp).", -1,
             false, 0, 0, 0, -1, EnchantmentRarity.RARE, List.of(EnchantmentsRecipients.ROD)),
 
-    GRAPPIN("Grappin", "§fPermet de propulser le joueur dans la direction de la rod (50 sec de cooldown).",
+    GRAPPIN("Grappin", "§8Permet de propulser le joueur dans la direction de la rod (50 sec de cooldown).",
             -1, false, 0, 0, 50, -1, EnchantmentRarity.EPIC, List.of(EnchantmentsRecipients.ROD));
 
     private final String displayName;
@@ -120,7 +120,7 @@ public enum Enchantments {
     }
 
     public String canBeAppliedOn() {
-        return "§fApplicable sur: " + recipients.stream().map(EnchantmentsRecipients::getName).reduce((s1, s2) -> s1 + ", " + s2).orElse("");
+        return "§7Applicable sur: " + recipients.stream().map(EnchantmentsRecipients::getName).reduce((s1, s2) -> s1 + ", " + s2).orElse("");
     }
 
     public String getDisplayName() {
@@ -185,7 +185,7 @@ public enum Enchantments {
             if (current >= maxLengthPerLine && c == ' ') {
                 components.add(Component.text(builder.toString()));
                 builder = new StringBuilder();
-                builder.append("§f");
+                builder.append("§8");
                 current = 0;
                 continue;
             }
@@ -200,7 +200,7 @@ public enum Enchantments {
             if (current >= maxLengthPerLine && c == ' ') {
                 components.add(Component.text(builder.toString()));
                 builder = new StringBuilder();
-                builder.append("§f");
+                builder.append("§7");
                 current = 0;
                 continue;
             }
@@ -210,9 +210,9 @@ public enum Enchantments {
         components.add(Component.text(builder.toString()));
 
         if (hasMaxLevel()) {
-            components.add(Component.text("§fNiveau maximum: " + RomanNumberUtils.toRoman(maxLevel)));
+            components.add(Component.text("§7Niveau maximum: " + RomanNumberUtils.toRoman(maxLevel)));
         } else {
-            components.add(Component.text("§fNiveau maximum: " + RomanNumberUtils.toRoman(1)));
+            components.add(Component.text("§7Niveau maximum: " + RomanNumberUtils.toRoman(1)));
         }
 
         return components;
