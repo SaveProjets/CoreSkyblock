d13d505055184d061cbe43952b86781015a217b6
Farmeurimmo <contact@farmeurimmo.fr>
2024-08-22 10:31:26 +0200

    Reduced pool size for Database connection

--- src/main/java/fr/farmeurimmo/coreskyblock/storage/DatabaseManager.java
+++ src/main/java/fr/farmeurimmo/coreskyblock/storage/DatabaseManager.java
@@ -40,7 +40,7 @@ public class DatabaseManager {
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         config.setLeakDetectionThreshold(5_000);
-        config.setMaximumPoolSize(10);
+        config.setMaximumPoolSize(5);
         config.setConnectionTimeout(60_000);
         ds = new HikariDataSource(config);
 
