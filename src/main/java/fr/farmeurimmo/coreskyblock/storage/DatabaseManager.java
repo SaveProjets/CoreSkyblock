788886b8fa5ece58fbfc60eaf937aeff8d6baedc
Farmeurimmo <contact@farmeurimmo.fr>
2024-08-24 10:58:26 +0200

    Going back to normal pool size

--- src/main/java/fr/farmeurimmo/coreskyblock/storage/DatabaseManager.java
+++ src/main/java/fr/farmeurimmo/coreskyblock/storage/DatabaseManager.java
@@ -40,7 +40,7 @@ public class DatabaseManager {
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         config.setLeakDetectionThreshold(5_000);
-        config.setMaximumPoolSize(5);
+        config.setMaximumPoolSize(10);
         config.setConnectionTimeout(30_000);
         config.setIdleTimeout(600_000);
         config.setMaxLifetime(1_800_000);
